3840cfe42cf30789b009c281e5ecf2c984298bac
John Barnette <jbarnette@gmail.com>
2010-12-18 19:06:47 -0800

    Remove assert_not_nil assumption.

--- lib/modelizer/validations.rb
+++ lib/modelizer/validations.rb
@@ -1,8 +1,8 @@
 module Modelizer
   module Validations
     def test_validations_for attribute, *validations
-      @klass ||= name.gsub(/Test$/, "").constantize
-      @model ||= ::Modelizer.underscore @klass.name
+      @klass ||= ::Modelizer.model_class_for self
+      @model ||= ::Modelizer.method_name_for @klass
 
       unless instance_methods.collect { |m| m.to_s }.include? "new_#{@model}"
         raise "no model template for #{@klass.name}"
@@ -27,7 +27,7 @@ module Modelizer
     def validation_lambda_for_uniqueness klass, model, attribute
       lambda do
         existing = klass.first
-        assert_not_nil existing, "There's at least one #{model} fixture."
+        assert existing, "There's at least one #{model} fixture."
 
         assert_invalid attribute,
           send("new_#{model}", attribute => existing.send(attribute))
